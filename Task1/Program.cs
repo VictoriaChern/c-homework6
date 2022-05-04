a11f3f70c1e2a0f7629dd9015f82cde0090d9b32
Viktoria.Cherniavskaya <victoria.kli1707@gmail.com>
2022-05-05 00:59:25 +0300

    add homework6

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+int[] array = askNumbers();
+int count = countAboveZero(array);
+WriteLine($"{string.Join(", ", array)} -> {count}");
+
+int[] askNumbers() {
+    Write("Введите числа через пробел: ");
+    return ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+
+int countAboveZero(int[] array) {
+    return array.Where(x => x > 0).Count();
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using static System.Console;
+
+Clear();
+printResult(askNumbers());
+
+int[] askNumbers() {
+    Write("Введите b1, k1, b2, k2 по порядку через пробел: ");
+    return ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+}
+
+void printResult(int[] array) {
+    double x = ((double) array[2] - array[0]) / ((double) array[1] - array[3]);
+    double y = array[3] * x + array[2];
+    WriteLine($"b1 = {array[0]}, k1 = {array[1]}, b2 = {array[2]}, k2 = {array[3]} -> ({x}; {y})");
+}
